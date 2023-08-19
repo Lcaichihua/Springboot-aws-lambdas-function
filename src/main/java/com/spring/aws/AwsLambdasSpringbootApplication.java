0b2b87c1183558d1bfad067350e51c6445e7efaa
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-08-19 00:17:50 -0500

    first commit

---
+++ src/main/java/com/spring/aws/AwsLambdasSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdasSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdasSpringbootApplication.class, args);
+	}
+
+}
