40dfd5ceff6b49283bf02376851d3ebc0715bdeb
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-08-19 02:25:40 -0500

    add 3 type functions

--- src/main/java/com/spring/aws/config/AwsLambdaConfig.java
+++ src/main/java/com/spring/aws/config/AwsLambdaConfig.java
@@ -1,15 +1,46 @@
 package com.spring.aws.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Configuration
 public class AwsLambdaConfig {
 
-public Supplier<String> greeting(){
-    return () -> {
-        return "Hello Word" ;
-    };
-}
+    @Bean(name = "saludar")
+    public Supplier<String> greeting() {
+        return () -> "Hello Word";
+    }
+
+    @Bean
+    public Consumer<String> printParam(){
+
+        return (param) -> {
+            System.out.println(param);
+        };
+    }
+    @Bean
+    public Function<String,String> receiveParam(){
+        return (param) -> {
+          String name =param.toUpperCase();
+          return name;
+        };
+    }
+
+    public Supplier<Map<String ,Object>> createCharacter(){
+        return ()-> {
+            Map<String, Object > character =new HashMap<>();
+            character.put("name","Goky");
+            character.put("healthPoints",100);
+            character.put("skill","Kame Hame Ha.");
+            return character;
+
+        };
+    }
 }
