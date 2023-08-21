ee859d42667f69d309d05186a9cbe2a2a4f462e6
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-08-20 23:56:45 -0500

    add Class Character

--- src/main/java/com/spring/aws/config/AwsLambdaConfig.java
+++ src/main/java/com/spring/aws/config/AwsLambdaConfig.java
@@ -52,4 +52,7 @@ public class AwsLambdaConfig {
             return "Personaje recibido";
         };
     }
+
+
+
 }
---
+++ src/main/java/com/spring/aws/domain/Character.java
@@ -0,0 +1,12 @@
+package com.spring.aws.domain;
+
+import lombok.Data;
+
+@Data
+public class Character {
+
+    private Long id;
+    private String name;
+    private Long healthPoints;
+    private String[] skills;
+}
